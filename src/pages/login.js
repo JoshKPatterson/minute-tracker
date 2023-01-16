0db6a338c51d24ddf25c44922dd129e773acb740
Joshua Patterson <joshkpatt@gmail.com>
2023-01-16 00:20:16 -0500

    Added more functionality to login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,14 +3,23 @@ import styles from '../styles/Login.module.css'
 const login = () => {
   return (
     <div className={styles.container}>
-        <h1>Minute Tracker</h1>
-        <h2>Login</h2>
+        <h1>Welcome Back</h1>
         <div className={styles.loginMenu}>
             <form>
                 <input className={styles.loginTextInput} type='text' placeholder='Email'></input>
                 <input className={styles.loginTextInput} type='text' placeholder='Password'></input>
+                <p className={styles.loginForgotPassword}>Forgot password?</p>
                 <button className={styles.loginButton}><p>Login</p></button>
             </form>
+        </div>
+        <div className={styles.loginOauthTitle}>
+          <span />
+          <p>Sign in with Google or Facebook</p>
+          <span />
+        </div>
+        <div className={styles.loginOauthOptions} >
+          <button className={styles.loginOauthButton}>Google</button>
+          <button className={styles.loginOauthButton}>Facebook</button>
         </div>
         <p>Don't have an account? <span>Sign up</span></p>
     </div>
